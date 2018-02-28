402b5d993f6216976717d12f2bb23c899a61e7e8
Sebastian N <sebastian.navas@ocsbstudent.ca>
2018-02-28 13:25:06 -0500

    Add function

--- main.lua
+++ main.lua
@@ -17,3 +17,17 @@ local myTitle = display.newText('Welcome to ICS20 Pizza\nWhat size of pizza woul
 local diameterTextField = native.newTextField(display.contentCenterX, display.contentCenterY - 200, 450, 120)
 diameterTextField.id = 'diameter text'
 local unitsMeasure = display.newText('Inch', 1365, 570, native.SystemFont, 120)
+
+-- Button
+local myButton = display.newImageRect('Assets/Sprites/Pizza-icon.png', 200, 200 )
+myButton.x = 1650
+myButton.y = 570
+myButton.id = 'initiator'
+
+-- Function 
+local function theCosts(event)
+	local subtotal
+	local HST
+	local Total
+
+	subtotal = 0.75+1.00+(tonumber(diameterTextField.text))
